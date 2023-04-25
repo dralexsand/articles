0d9f18fd5e89f99adaaed738fb19de93df037ba0
dralexsand <dr.alex.sand.111@gmail.com>
2023-04-25 18:56:24 +0600

    add models, migrations

---
+++ backend/app/Models/Author.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function authors()
+    {
+        return $this->hasMany(Author::class);
+    }
 }
